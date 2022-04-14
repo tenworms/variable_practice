2957423e5600e8a15d07a91eac2623823574eb80
Andre Pedro <andrerpedro170@gmail.com>
2022-04-13 20:18:23 -0600

    annotations

--- variables.rb
+++ variables.rb
@@ -1,10 +1,13 @@
+# new owners information
 email = 'papajhons@gmail'
 name = 'jhon papa'
 age = '207'
+#nationwide
 chaines_owned = '4876'
 checking_account = '7823467824678246782364723.50'
 pizzas_created = '29734697264.75'
 best_pizza = 'false'
 cardboard_taste = 'true'
+# new owner took over
 email = 'pizzahut@gmail.com'
 name = 'bob hut'
